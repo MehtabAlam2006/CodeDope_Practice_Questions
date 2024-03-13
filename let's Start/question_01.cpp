ebf73586e4e22350a00eb7f1058299788dd4e7a4
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-13 22:50:42 +0500

    question_01

---
+++ let's Start/question_01.cpp
@@ -0,0 +1,19 @@
+// Author: Mehtab Alam
+// Github: MehtabAlam2006
+
+/*Write a C++ program to print
+*
+**
+***
+****
+on screen.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"*"<<endl;
+    cout<<"**"<<endl;
+    cout<<"***"<<endl;
+    cout<<"**** \n";
+ 
+}
