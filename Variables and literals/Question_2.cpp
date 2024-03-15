432c4efc42391e337a7bc852e094672d48f3aeaa
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-15 23:32:50 +0500

    Question 1, 2 done

---
+++ Variables and literals/Question_2.cpp
@@ -0,0 +1,13 @@
+// Author : Mehtab Alam
+// github : MehtabAlam2006
+
+/* Q.2 Store two integers in two variables x and y. Print the product of the two.*/
+#include <iostream>
+using namespace std;
+
+int main(){
+ int x = 3;
+ int y = 6;
+ int z = (x*y);
+ cout<<" the product of x and y = "<<z;
+}
---
+++ Variables and literals/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+// Author : Mehtab Alam
+// Github : MehtabAlam2006
+
+/* Q.1 Store two integers in two variables x and y. Print the sum of the two.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+ int x = 30;
+ int y = 10;
+ int z = x+y;
+ cout<<" the sum of x and y = "<<z;
+}
