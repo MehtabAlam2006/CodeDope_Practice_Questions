85e12cebb767b10c6c4ca2f8c7690d64121025da
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 19:21:49 +0500

    Question_10 done

---
+++ Variables and literals/Question_10.cpp
@@ -0,0 +1,12 @@
+// Author : Mehtab Alam
+// Github : MehtabAlam2006
+/*10.
+Write a program to add an integer variable having value 5 and a double variable having value 6.2. */
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    double y = 6.2;
+    double z = x + y;
+    cout<<" The sum of x and y : "<<z;
+}
