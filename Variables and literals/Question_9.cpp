9db4bef2c5e1e02c4379e04610f6615eb8b38ab6
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 19:03:00 +0500

    Question_9 done

---
+++ Variables and literals/Question_9.cpp
@@ -0,0 +1,15 @@
+// Author : Mehtab Alam
+// Github : MehtabAlam2006
+
+/*
+9.
+Write a program to add 3 to the ASCII value of the character 'd' and print the equivalent character. */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char f = 'd'+3;
+	cout << f << endl;
+
+}
