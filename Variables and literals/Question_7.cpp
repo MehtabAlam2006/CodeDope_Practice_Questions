2d65348608c51a0d5d72015d73c92d9fbcd6624e
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 18:47:02 +0500

    Question_7 done

---
+++ Variables and literals/Question_7.cpp
@@ -0,0 +1,12 @@
+// Auhtor : Mehtab Alam
+// Github: MehtabAlam2006
+/*
+Q7.
+Print the ASCII value of the character 'h'.*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    char D = 'h';
+    cout<<"The ASCII value of h is = "<< (int)D;
+}
