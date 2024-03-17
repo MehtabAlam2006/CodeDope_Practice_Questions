cd738163cdc8ca03d9ecd6cdec5cf525c5798435
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 18:54:21 +0500

    Question_8 done

---
+++ Variables and literals/Question_8.cpp
@@ -0,0 +1,11 @@
+// Author : Mehtab Alam
+// Github : MehtabAlam2006
+/* Q.8
+Write a program to assign a value of 100.235 to a double variable and then convert it to int. */
+#include <iostream>
+using namespace std;
+
+int main(){
+    double D = 100.235 ;
+    cout<< (int)D;
+}
