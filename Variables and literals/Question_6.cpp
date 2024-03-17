6c4457c34346975d8c99a2c8fc7295381608590a
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 18:36:46 +0500

    Question_6 done

---
+++ Variables and literals/Question_6.cpp
@@ -0,0 +1,16 @@
+// Author : Mehtab Alam
+// Github : MehtabAlam2006
+/*Q.6 Write a C++ program to print an int, a double and a char on screen.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    double y = 5.555555;
+    char d = 'G';
+    cout<<" The integer is : "<<x<<endl;
+    cout<<" The double is :" <<y<<endl;
+    cout<<" The char is: "<<d;
+
+}
