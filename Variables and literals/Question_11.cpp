ce27bb253a323c78a2f26607de9cf3a7073d7982
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-17 19:27:52 +0500

    Question_11 done

---
+++ Variables and literals/Question_11.cpp
@@ -0,0 +1,12 @@
+// Auhtor 
+// Github : MehtabAlam2006
+/*11.
+Write a program to find the square of the number 3.9. */
+
+#include <iostream>
+using namespace std;
+int main (){
+    double x = 3.9;
+    double y = x * x;
+    cout<<" the square of 3.9 is :"<<y; 
+}
