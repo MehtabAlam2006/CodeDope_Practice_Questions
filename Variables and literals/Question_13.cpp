5258cd9f3fe23ce8052baa8a3bc59a0d3f3e1ddb
Mehtab Alam <iammehtabalam@outlook.com>
2024-03-18 21:09:58 +0500

    Question_13 done

---
+++ Variables and literals/Question_13.cpp
@@ -0,0 +1,17 @@
+/* 
+Take a char input from user and print it's ASCII value. */
+//  Author : Mehtab Alam
+// Github : MehtabAlam2006
+
+
+ #include <iostream>
+ using namespace std;
+
+ int main()
+ {
+  char C;
+  cout<<" enter your char : ";
+  cin>>C;
+  cout<<"the ASCII value of the given char is: "<<(int)'C';
+
+ }
